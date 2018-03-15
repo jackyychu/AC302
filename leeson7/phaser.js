742f85d7c9f5b387041ae01adc99c2f9c09b59c9
Jacky <jacky@firstcodeacademy.com>
2018-03-15 14:36:53 +0800

    update

--- leeson7/phaser.js
+++ leeson7/phaser.js
@@ -9,7 +9,7 @@ function preload(){
 	game.load.image("ground", "assets/platform.png");
 	game.load.image("star", "assets/star.png");
 	game.load.spritesheet("dude", "assets/dude.png", 32, 48);
-	game.load.spritesheet("baddie", "assets/baddie.png", 32, 32);
+	game.load.spritesheet("enemey1", "assets/enemey1.png", 32, 32);
 }
 
 function create(){
@@ -19,7 +19,7 @@ function create(){
 	game.add.sprite(0, 0, "sky");
 
 	//Create a group of platform
-	var platforms = game.add.physicsGroup();
+	platforms = game.add.physicsGroup();
 	platforms.enableBody = true;
 
 	//Create the ground
@@ -34,18 +34,18 @@ function create(){
 	ledge.body.immovable = true;
 
 	//set text style
-	var style = {font:"bold 32px Arial", fill:"#fff"};
+	style = {font:"bold 32px Arial", fill:"#fff"};
 
 	//adding the score
-	var scorelabel = game.add.text(300, 560, "Score: ", style);
-	var scoretext = game.add.text(420, 560, score, style);
+	scorelabel = game.add.text(300, 560, "Score: ", style);
+	scoretext = game.add.text(420, 560, score, style);
 
 	//add the lives
-	var lifelabel = game.add.text(10, 5, "life: ", style);
-	var lifetext = game.add.text(120, 5, life, style);
+	lifelabel = game.add.text(10, 5, "life: ", style);
+	lifetext = game.add.text(120, 5, life, style);
 
 	//add the dude
-	var player = game.add.sprite(20, 400,"dude");
+	player = game.add.sprite(20, 400,"dude");
 
 	player.animations.add("left", [0, 1 ,2 ,3], 10, true);
 	player.animations.add("right", [5, 6 ,7 ,8], 10, true);
@@ -54,18 +54,18 @@ function create(){
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
 
-	//add the baddie
-	var baddie = game.add.sprite(760, 20,"baddie");
+	//add the enemey1
+	enemey1 = game.add.sprite(760, 20,"enemey1");
 
-	baddie.animations.add("left", [0, 1], 10, true);
-	baddie.animations.add("right", [2, 3], 10, true);
-	game.physics.arcade.enable(baddie);
-	baddie.body.bounce.y = 0.2;
-	baddie.body.gravity.y = 500;
-	baddie.body.collideWorldBounds = true;
+	enemey1.animations.add("left", [0, 1], 10, true);
+	enemey1.animations.add("right", [2, 3], 10, true);
+	game.physics.arcade.enable(enemey1);
+	enemey1.body.bounce.y = 0.2;
+	enemey1.body.gravity.y = 500;
+	enemey1.body.collideWorldBounds = true;
 
 	//add the stars
-	var stars = game.add.physicsGroup();
+	stars = game.add.physicsGroup();
 	stars.enableBody = true;
 
 	for (var i = 0; i < 12; i++){
@@ -82,7 +82,7 @@ function create(){
 function update(){
 
 	game.physics.arcade.collide(player, stars);
-	game.physics.arcade.collide(baddie, platforms);
+	game.physics.arcade.collide(enemey1, platforms);
 	game.physics.arcade.collide(player, platforms);
 
 	player.body.velocity.x = 0;
@@ -103,13 +103,14 @@ function update(){
 	}
 
 	game.physics.arcade.overlap(player,stars, collectStar);
-	game.physics.arcade.overlap(player, baddie, loseLife);
+	game.physics.arcade.overlap(player, enemey1, loseLife);
 
-	moveBaddie();
+	moveenemey1();
 
 	if (lives <= 0){
 		endGame();
 	}
+}
 
 function endGame(){
 	player.kill();
@@ -118,3 +119,32 @@ function endGame(){
 	lifelabel.visible = false;
 	lifetext.visible = false;
 }
+
+function moveenemey1(){
+	if (enemey1.x > 759){
+		enemey1.animations.play("left");
+		enemey1.body.velocity.x = -120;
+	} else if (enemey1.x < 405){
+		enemey1.animations.play("right");
+		enemey1.body.velocity.x = 120;
+	}
+
+}
+
+function collectStar(player,star){
+	score +=1;
+	star.kill();
+
+	scoretext.setText(score);
+	star.reset(Math.random()*750, 0);
+}
+
+function loseLife(player, enemey1){
+	lives -=1;
+	lifetext.setText(lives);
+
+	enemey1.kill();
+	enemey1.reset(10,20);
+}
+
+
